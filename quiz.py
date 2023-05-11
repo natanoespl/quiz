9c8760364d7a58d6268cb940c6a330a60e90aced
natanoespl <132944298+natanoespl@users.noreply.github.com>
2023-05-11 10:26:19 -0300

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,50 @@
+print("Seja bem vindo ao meu QUIZ!")
+
+nome = input("Antes de tudo, qual o seu nome? ")
+print("Olá, " +nome)
+
+jogando = input("Você quer jogar? ")
+
+if jogando.lower() != "sim":
+    quit()
+
+print("Que bom! Vamos jogar :)")
+score = 0
+
+resposta = input("Qual o significado da sigla CPU? ")
+if resposta.lower() == "central processing unit":
+    print("Correto!")
+    score += 1
+else:
+    print("Incorreto!")
+
+resposta = input("Qual o significado da sigla GPU? ")
+if resposta.lower() == "graphics processing unit":
+    print("Correto!")
+    score += 1
+else:
+    print("Incorreto!")
+
+resposta = input("Qual o significado da sigla RAM? ")
+if resposta.lower() == "random access memory":
+    print("Correto!")
+    score += 1
+else:
+    print("Incorreto!")
+
+resposta = input("Qual o significado da sigla PSU? ")
+if resposta.lower() == "power supply":
+    print("Correto!")
+    score += 1
+else:
+    print("Incorreto!")    
+
+if score <= 1: 
+    print("Parabéns," ,nome, ("você acertou " + str(score) + " questão!"))
+    print("A sua porcentagem de acertos foi de: " +str((score / 3) * 100) + "%.")
+else:
+    print("Parabéns," ,nome, ("você acertou " + str(score) + " questões!"))
+    print("A sua porcentagem de acerto foi de: " +str((score / 3) * 100) + "%.")
+
+if score < 1:
+    print ("Infelizmente você errou todas as questões!")
